7e8f910cd4d6f8c1e7d9cd5a10d293928dc5f641
vodobryshkin <dobryshkinvladimir@yandex.ru>
2026-06-01 11:05:59 +0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    java
+    id("org.springframework.boot") version "4.0.6"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.github.vodobryshkin"
+version = "0.0.1-SNAPSHOT"
+description = "CauchyProblem"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-graphql")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-graphql-test")
+    testCompileOnly("org.projectlombok:lombok")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testAnnotationProcessor("org.projectlombok:lombok")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/test/java/com/github/vodobryshkin/cauchyproblem/CauchyProblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vodobryshkin.cauchyproblem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CauchyProblemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
